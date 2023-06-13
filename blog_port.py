70b827b444410979e0e00d30c4d43f505ce9bc2a
Jude <jude.rendel.hall@gmail.com>
2023-06-13 16:43:15 -0600

    Progress

--- blog_port.py
+++ blog_port.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 from urllib.parse import urlparse
 
+"""
 # create a function to extract the visible text from the web page
 def extract_visible_text(soup):
     text = [element.text.replace('\n', ' ') for element in soup.find_all(['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'a', 'li'])]
@@ -10,14 +11,29 @@ def extract_visible_text(soup):
     # count the words in the text_string
     word_count = len(text_string.split())
     print(word_count)
+"""
 
-# create a function to extract specific text from the web page with tags 
+# create a function to extract specific tags from a blog, and store them in a dictionary in the order they appear
 def extract_specific_text(soup):
-    tags = ['h1', 'h2',]
+    tags = ['a','h1', 'h2', 'h3', 'li', 'p',]
+    
+    exclude_header = soup.find('div', {'data-elementor-type': 'header'})
+    exclude_footer = soup.find('div', {'data-elementor-type': 'footer'})
+    
     dict = {}
     x = 0
-
+    
     for element in soup.find_all(tags):
+        if element in exclude_header.descendants:
+            continue
+        if element in exclude_footer.descendants:
+            continue
+        """
+        if element.get('data-elementor-type') in ['header', 'footer']:
+            continue
+        if element.get('data-element_type') in ['column']:
+            continue
+        """
         dict [x] = [element.name, element.text]
         x += 1
     print(dict)
@@ -51,13 +67,3 @@ if response.status_code == 200:
     extract_specific_text(soup)
 else:
     print("Failed to fetch the web page. Status code:", response.status_code)
-
-"""
-if response.status_code == 200:
-    soup = BeautifulSoup(response.text, 'html.parser')
-    extract_slug(web_address)
-    extract_visible_text(soup)
-    extract_image_urls(soup)
-else:
-    print("Failed to fetch the web page. Status code:", response.status_code)
-"""
