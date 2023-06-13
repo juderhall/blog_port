73e651842e068c86277814468039a13ae9a792fd
Jude <jude.rendel.hall@gmail.com>
2023-06-13 17:28:36 -0600

    Progress

--- blog_port.py
+++ blog_port.py
@@ -2,40 +2,26 @@ from bs4 import BeautifulSoup
 import requests
 from urllib.parse import urlparse
 
-"""
-# create a function to extract the visible text from the web page
-def extract_visible_text(soup):
-    text = [element.text.replace('\n', ' ') for element in soup.find_all(['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'a', 'li'])]
-    text_string = ' '.join(text)
-    print(text_string)
-    # count the words in the text_string
-    word_count = len(text_string.split())
-    print(word_count)
-"""
-
 # create a function to extract specific tags from a blog, and store them in a dictionary in the order they appear
 def extract_specific_text(soup):
     tags = ['a','h1', 'h2', 'h3', 'li', 'p',]
     
     exclude_header = soup.find('div', {'data-elementor-type': 'header'})
     exclude_footer = soup.find('div', {'data-elementor-type': 'footer'})
+    exclude_widget = soup.find('section', {'class': 'elementor-section elementor-inner-section elementor-element elementor-element-11e3c129 elementor-hidden-mobile elementor-section-boxed elementor-section-height-default elementor-section-height-default elementor-sticky elementor-sticky--active elementor-section--handles-inside elementor-sticky--effects'})
     
     dict = {}
     x = 0
-    
+
     for element in soup.find_all(tags):
         if element in exclude_header.descendants:
             continue
         if element in exclude_footer.descendants:
             continue
-        """
-        if element.get('data-elementor-type') in ['header', 'footer']:
-            continue
-        if element.get('data-element_type') in ['column']:
-            continue
-        """
+    
         dict [x] = [element.name, element.text]
         x += 1
+        
     print(dict)
 
 # create a function to extract the image urls from the web page
