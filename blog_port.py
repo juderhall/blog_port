a3738d7c3c51877adf49b0af47cb057aa723b74b
Jude <jude.rendel.hall@gmail.com>
2023-06-13 18:09:23 -0600

    progress

--- blog_port.py
+++ blog_port.py
@@ -1,34 +1,22 @@
-from blog_scrape import extract_specific_text
+from blog_scrape import extract_specific_text, extract_slug
 from bs4 import BeautifulSoup
 import requests
-from urllib.parse import urlparse
-    
-# create a function to extract the image urls from the web page
-def extract_image_urls(soup):
-    images = [img['src'] for img in soup.find_all('img') if 'src' in img.attrs]
-    for image in images:
-        print(image)
-
-# create a function to extract the slug from the web address
-def extract_slug(web_address):
-    parsed_url = urlparse(web_address)
-    slug = parsed_url.path
-    print(slug)
-
-# create varible to hold the url
-web_address = "https://zenpayments.com/virtual-merchant/"
 
 # create a variable to hold the headers
 headers = {
     'User-Agent' : 'Blog Scraper (https://github.com/juderhall/codespaces-get_blog_info)',
     'Email' : 'jude@zenpayments.com'
-}
+}   
+
+# create varible to hold the url
+web_address = "https://zenpayments.com/virtual-merchant/"
 
 # create a variable to hold the response
 response = requests.get(web_address, headers=headers)
 
 if response.status_code == 200:
     soup = BeautifulSoup(response.text, 'html.parser')
+    extract_slug(web_address)
     extract_specific_text(soup)
 else:
     print("Failed to fetch the web page. Status code:", response.status_code)
--- blog_scrape.py
+++ blog_scrape.py
@@ -1,4 +1,12 @@
-#
+from urllib.parse import urlparse
+
+# create a function to extract the slug from the web address
+def extract_slug(web_address):
+    parsed_url = urlparse(web_address)
+    slug = parsed_url.path
+    print(slug)
+
+# scrapes content off blog page and prints to console 
 def extract_specific_text(soup):
     tags = ['a','h1', 'h2', 'h3', 'li', 'p',]
     
